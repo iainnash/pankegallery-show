424589a674aa778b129376ef6f0e38f0ce3ae4ea
inpublicspace <157924758+inpublicspace@users.noreply.github.com>
2024-07-16 11:51:00 -0400

    Update [id].tsx

--- pages/exhibit/drawing-machine/[id].tsx
+++ pages/exhibit/drawing-machine/[id].tsx
@@ -6,7 +6,7 @@ const DrawingMachinePiece = ({ id }: any) => {
   const nft = data[parseInt(id)];
   return (
     <div style={{ padding: "20px" }}>
-      <Head title={nft.name} ogImage={nft.image.replace('ipfs://', 'https://cloudflare-ipfs.com/ipfs/')} />
+      <Head title={nft.name} ogImage={`https://wsrv.nl/?w=512&url=${nft.image.replace('ipfs://', process.env.NEXT_PUBLIC_IPFS_HOST)}`} />
       {/* <Markdown markdown={drawingMachine} /> */}
       <ShowNFT
         contract={process.env.NEXT_PUBLIC_DRAWING_MACHINE_CONTRACT_ADDRESS}
