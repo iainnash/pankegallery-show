66b02431aef17474d12e5408ddecaf93cfe4d87d
iain nash <me@iain.in>
2025-09-08 12:34:07 -0400

    import fixes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  compiler: {
-    emotion: true,
-  },
-  images: {
-    domains: ['cloudflare-ipfs.com'],
-    remotePatterns: [{hostname: 'cloudflare-ipfs.com'}]
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
